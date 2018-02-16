d07637e0644c984786ff62a8ebbd70b1e0d0846f
Zach Cobell <zachary.cobell@arcadis.com>
2018-02-16 13:31:37 -0500

    Updating python test file with more examples

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # ADCIRCModules
-Implementation of routines used to manipulate ADCIRC model files in C++
+Implementation of routines used to manipulate ADCIRC model files in C++. The code provides a C++ interface as well as a Python interface.
+
+## Python
+When the python interface is compiled, you will have a PyAdcirc library file. For windows, this will by `PyAdcirc.dll` and for Linux type systems this will be `_PyAdcirc.so`. In addition to the library, you will see `PyAdcirc.py`. CMake will attempt to install these files to the appropriate directory, however, the user can change this. The standard library (`adcircmodules.dll` or `adcircmodules.so` is also required to be in a location where it can be found at runtime (`LD_LIBRARY_PATH` on Unix systems).
+
+```
+import PyAdcirc
+m = PyAdcirc.Mesh("mymesh.grd")
+error = m.read()
+```
 
 ## Note
 This project is still under development.
@@ -11,7 +20,7 @@ This project is still under development.
 | Library  | Function | Source |
 |----------|----------|--------|
 | KDTREE2  | Used for efficient nearest neighbor searches | Matthew B. Kennel, Institute for Nonlinear Science, UCSD (2004) |
-| Boost    | Used as part of KDTREE2 | [Visit Website](http://www.boost.org/) |
+| Boost    | Used by KDTREE2 and for fast file I/O | [Visit Website](http://www.boost.org/) |
 | Proj4    | Used to project to various coordinate systems |[Visit Website](https://trac.osgeo.org/proj/) |
 | netCDF   | Used to read ADCIRC files formatted with this library | [Visit Website](http://www.unidata.ucar.edu/software/netcdf/) |
 | shapelib | Used to convert meshes to shapefile format | [Visit Website](http://shapelib.maptools.org/) |
--- testing/python_tests/pyadcirc_testcase.py
+++ testing/python_tests/pyadcirc_testcase.py
@@ -2,8 +2,8 @@
 import PyAdcirc
 
 print "Reading a mesh"
-m=PyAdcirc.Mesh("../testing/test_files/ms-riv.grd")
-ierr=m.read()
+m = PyAdcirc.Mesh("../testing/test_files/ms-riv.grd")
+ierr = m.read()
 if ierr != 0:
     exit(ierr)
 
@@ -15,19 +15,36 @@ print "  Node 2 location: ",m.node(1).x(),",",m.node(1).y(),",",m.node(1).z()
 print "  Shifting node..."
 m.node(1).setX(-77.045)
 print "  New node location: ",m.node(1).x(),",",m.node(1).y(),",",m.node(1).z()
-print "  Projecting mesh to utm-15 (epsg 26915)..."
-#ierr=m.reproject(26916)
-#if ierr != 0:
-#    exit(ierr)
-#print "  Mesh projected successfully."
-#print "  New node position: ",m.node(1).x(),",",m.node(1).y(),",",m.node(1).z()
-
-print "Reading a fort.13"
-f=PyAdcirc.NodalAttributes("../testing/test_files/ms-riv.13",m)
-ierr=f.read()
+print "  Generating a search tree for the mesh..."
+ierr = m.buildNodalSearchTree()
 if ierr != 0:
     exit(ierr)
+print "  Nodal searh tree generated."
+print "  Searching for a node..."
+index = m.nodalSearchTree().findNearest(-95.0,27.0)
+print "  Found index: ",index
+print "  Projecting mesh to utm-15 (epsg 26915)..."
+ierr = m.reproject(26916)
+if ierr != 0:
+    exit(ierr)
+print "  Mesh projected successfully."
+print "  New node position: ",m.node(1).x(),",",m.node(1).y(),",",m.node(1).z()
+print "  Writing the utm-15 mesh"
+ierr = m.write("ms-riv-utm15.grd")
+if ierr != 0:
+    exit(ierr)
+print "  Mesh written in utm-15 successfully"
+print "  Writing mesh as a shapefile..."
+ierr = m.toShapefile("ms-riv-utm15.shp");
+if ierr != 0:
+    exit(ierr)
+print "  Mesh written in utm-15 shapefile successfully"
 
+print "Reading a fort.13"
+f = PyAdcirc.NodalAttributes("../testing/test_files/ms-riv.13",m)
+ierr = f.read()
+if ierr != 0:
+    exit(ierr)
 print "Fort13 read successfully"
 print "  Manning n at node",m.node(1).id(),": ",f.attribute("mannings_n_at_sea_floor",1).value(0)
 print "  Tau0 at node",m.node(1).id(),": ",f.attribute("primitive_weighting_in_continuity_equation",1).value(0)
