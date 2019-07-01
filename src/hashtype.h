2558e0c8cabbabb1206e1bd318815fa706e035c4
Zach Cobell <zcobell@gmail.com>
2019-07-01 00:35:18 -0400

    Setting default hash type to SHA1

--- src/hashtype.h
+++ src/hashtype.h
@@ -3,6 +3,6 @@
 
 enum HashType { AdcmodMD5, AdcmodSHA1, AdcmodSHA256 };
 
-constexpr HashType AdcircDefaultHash = HashType::AdcmodMD5;
+constexpr HashType AdcircDefaultHash = HashType::AdcmodSHA1;
 
 #endif // HASHTYPE_H
