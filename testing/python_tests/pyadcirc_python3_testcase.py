3da03c83ada367c300ec8385a24b703934e07512
Zach Cobell <zcobell@gmail.com>
2020-11-30 16:20:28 -0500

    Adding internal build of proj and renaming from PyAdcirc to adcircmodules for python import
    Others have decided PyAdcirc is a cool name and
    named their packages the same thing. Since they
    intentionally chose ambiguity, I've chosen disambiguation

--- testing/python_tests/pyadcirc_python3_testcase.py
+++ testing/python_tests/pyadcirc_python3_testcase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import PyAdcirc
+import adcircmodules
 import math
 
 print("Reading a mesh")
-m = PyAdcirc.Mesh("../testing/test_files/ms-riv.grd")
+m = adcircmodules.Mesh("../testing/test_files/ms-riv.grd")
 m.read()
 
 print("Mesh read successfully")
@@ -34,26 +34,26 @@ print("  Getting mesh xyz as python array...")
 xyz=m.xyz();
 print("  Accessing data in python array, node position is: ",xyz[0][1],",",xyz[1][1],",",xyz[2][1])
 print("Reading a fort.13")
-f = PyAdcirc.NodalAttributes("../testing/test_files/ms-riv.13",m)
+f = adcircmodules.NodalAttributes("../testing/test_files/ms-riv.13",m)
 f.read()
 print("Fort13 read successfully")
 print("  Manning n at node",m.node(1).id(),": ",f.attribute("mannings_n_at_sea_floor",1).value(0))
 print("  Tau0 at node",m.node(1).id(),": ",f.attribute("primitive_weighting_in_continuity_equation",1).value(0))
 print("Reading an adcirc ascii output file")
-o = PyAdcirc.ReadOutput("../testing/test_files/maxele.63")
+o = adcircmodules.ReadOutput("../testing/test_files/maxele.63")
 o.open();
 o.read();
 o.close()
 print("   Output value for record 1 at node 42: ",o.data(0).z(41))
 print("Adcirc maxele ascii file read successfully")
 print("Reading netcdf maxele file")
-onc = PyAdcirc.ReadOutput("../testing/test_files/maxele.63.nc")
+onc = adcircmodules.ReadOutput("../testing/test_files/maxele.63.nc")
 onc.open()
 onc.read()
 onc.close();
 print("   Output value for reacord 1 at node 42: ",onc.data(0).z(41))
 print("Reading ascii timeseries scalar file")
-asc = PyAdcirc.ReadOutput("../testing/test_files/sparse_fort.63")
+asc = adcircmodules.ReadOutput("../testing/test_files/sparse_fort.63")
 asc.open();
 asc.read();
 asc.read();
@@ -61,7 +61,7 @@ asc.read();
 print("    Output value for record 3 at node 925: ",asc.data(2).z(1220));
 asc.close();
 print("Reading timeseries netcdf vector file")
-ncv=PyAdcirc.ReadOutput("../testing/test_files/fort.64.nc")
+ncv=adcircmodules.ReadOutput("../testing/test_files/fort.64.nc")
 ncv.open();
 ncv.read();
 ncv.read();
@@ -69,11 +69,11 @@ ncv.clearAt(1); #...deletes record 2 from memory
 ncv.read();
 print("    Velocity at node 1221: ",ncv.data(2).u(1220),",",ncv.data(2).v(1220))
 print("       Magnitude is: ",ncv.data(2).magnitude(1220))
-print("       Direction is: ",ncv.data(2).direction(1220,PyAdcirc.Radians), "radians or ",ncv.data(2).direction(1220,PyAdcirc.Degrees)," degrees")
+print("       Direction is: ",ncv.data(2).direction(1220,adcircmodules.Radians), "radians or ",ncv.data(2).direction(1220,adcircmodules.Degrees)," degrees")
 print("Success reading netcdf vector timeseries file")
 ncv.close()
 print("Reading harmonics elevation file")
-he = PyAdcirc.HarmonicsOutput("../testing/test_files/fort.53");
+he = adcircmodules.HarmonicsOutput("../testing/test_files/fort.53");
 he.read();
 print("Harmonics elevation read successfully")
 print("   M2 amplitude at node 1: ",he.amplitude("M2").value(he.nodeIdToArrayIndex(1)))
@@ -81,7 +81,7 @@ print("   M2 phase at node 1: ",he.phase("M2").value(he.nodeIdToArrayIndex(1)))
 print("   K1 amplitude at node 1: ",he.amplitude("K1").value(he.nodeIdToArrayIndex(1)))
 print("   K1 phase at node 1: ",he.phase("K1").value(he.nodeIdToArrayIndex(1)))
 print("Reading harmonics velocity file")
-hv = PyAdcirc.HarmonicsOutput("../testing/test_files/fort.54");
+hv = adcircmodules.HarmonicsOutput("../testing/test_files/fort.54");
 hv.read();
 print("Harmonics velocity read successfully")
 print("   M2 u magnitude at node 1: ",hv.u_amplitude("M2").value(hv.nodeIdToArrayIndex(1)))
