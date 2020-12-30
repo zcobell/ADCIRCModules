459a9f15d564e8a14b25907c7504cab5295b455b
Zach Cobell <zcobell@gmail.com>
2020-12-30 11:48:29 -0500

    Cleaning up progress bar class

--- src/progressbar.cpp
+++ src/progressbar.cpp
@@ -28,7 +28,6 @@ ProgressBar::ProgressBar(size_t total_iterations, const std::string &prefix)
     : m_display_mode(istty()),
       m_total_iterations(total_iterations),
       m_current(0),
-      m_updateInterval(250),
       m_prefix(prefix) {}
 
 ProgressBar::~ProgressBar() = default;
@@ -54,12 +53,13 @@ void ProgressBar::begin() {
     m_indicators_progress->set_option(indicators::option::Remainder{" "});
     m_indicators_progress->set_option(indicators::option::ShowPercentage(true));
     m_indicators_progress->set_option(
+        indicators::option::MaxProgress(m_total_iterations));
+    m_indicators_progress->set_option(
         indicators::option::ShowElapsedTime(true));
     m_indicators_progress->set_option(
-        indicators::option::ShowRemainingTime(false));
+        indicators::option::ShowRemainingTime(true));
     m_indicators_progress->set_option(indicators::option::PrefixText(m_prefix));
     m_current = 0;
-    m_lastUpdate = std::chrono::high_resolution_clock::now();
   } else {
     m_boost_progress =
         std::make_unique<boost::progress_display>(m_total_iterations);
@@ -68,11 +68,6 @@ void ProgressBar::begin() {
 
 void ProgressBar::end() {
   if (m_display_mode == 1) {
-    this->m_indicators_progress->set_option(
-        indicators::option::PostfixText(""));
-    m_indicators_progress->set_option(
-        indicators::option::ShowRemainingTime(false));
-    m_indicators_progress->set_progress(100);
     indicators::show_console_cursor(true);
     m_indicators_progress.reset(nullptr);
   } else {
@@ -84,23 +79,18 @@ void ProgressBar::tick() {
   std::lock_guard<std::mutex> guard(m_mutex);
   if (m_display_mode == 1) {
     m_current++;
-    auto now = std::chrono::high_resolution_clock::now();
-    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(
-                  now - m_lastUpdate)
-                  .count();
-    if (dt > m_updateInterval) {
-      m_lastUpdate = now;
-      double progress_pct =
-          ((double)m_current / (double)m_total_iterations) * 100.0;
+    if (m_current < m_total_iterations) {
       std::string postfix_string =
           std::to_string(m_current) + "/" + std::to_string(m_total_iterations);
       this->m_indicators_progress->set_option(
           indicators::option::PostfixText(postfix_string));
-      if (progress_pct >= 1.0)
-        m_indicators_progress->set_option(
-            indicators::option::ShowRemainingTime(true));
-      this->m_indicators_progress->set_progress(progress_pct);
+    } else {
+      this->m_indicators_progress->set_option(
+          indicators::option::PostfixText(""));
+      m_indicators_progress->set_option(
+          indicators::option::ShowRemainingTime(false));
     }
+    m_indicators_progress->tick();
   } else {
     ++(*m_boost_progress);
   }
